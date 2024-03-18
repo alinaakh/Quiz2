ec5b657d8614562fe62567c7a150493348741956
alinaakh <161269953+alinaakh@users.noreply.github.com>
2024-03-18 17:06:18 +0900

    Update Math.fs

--- Math.fs
+++ Math.fs
@@ -6,4 +6,10 @@ open System
 /// 32-bit integer, and returns the number of steps it takes to reach 1 in the
 /// Collatz sequence.
 let collatz n =
+  let rec iter n counter = 
+    if n = 1 then counter
+    elif n % 2 = 0 then iter (n / 2) (counter + 1)
+    else iter (n * 3 + 1) (counter + 1)
+  iter n 0
+collatz 5
   0
