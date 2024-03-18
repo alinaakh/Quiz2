aa8e20886a30b207cd5018abd6988e919a121cf1
alinaakh <161269953+alinaakh@users.noreply.github.com>
2024-03-18 17:13:37 +0900

    Update Program.fs

--- Program.fs
+++ Program.fs
@@ -7,5 +7,11 @@ let main _args =
   printfn "You can test your program by directly modifying Program.fs"
   printfn "Or, you can change the unit tests in the `tests` directory."
   printfn "---"
-  collatz 4 |> printfn "# of steps to reach one = %d"
-  0
+let collatz n =
+  let rec iter n counter = 
+    if n = 1 then counter
+    elif n % 2 = 0 then iter (n / 2) (counter + 1)
+    else iter (n * 3 + 1) (counter + 1)
+  iter n 0
+collatz 4 |> printfn "# of steps to reach one = %d"
+  
